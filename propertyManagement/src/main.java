30a62591769617371b9336e704f04b4267b7814e
hloni95 <mltau95@gmail.com>
2024-03-27 09:21:15 +0200

    The driver class for this project

---
+++ propertyManagement/src/main.java
@@ -0,0 +1,18 @@
+/*
+    
+ */
+
+/**
+ *
+ * @author anonza
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
