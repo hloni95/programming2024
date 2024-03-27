7f5bc85401906fd9f1bb3cd569a037988387b1c7
hloni95 <mltau95@gmail.com>
2024-03-27 18:49:51 +0200

    This class manages tenant information

---
+++ propertyManagement/src/tenants.java
@@ -0,0 +1,23 @@
+/*
+ *  
+    Tenants approach the agent and are added to a waiting list.
+    The following details of the first 10 candidates are kept:
+        idNum
+        sal per month
+        rating (nth Fibinacci number's factorial, where n is the remainder 
+        of dividing)
+
+objectives: 
+        a. create an appropriate data structure to store candidate tenats that 
+           are in the wait list
+        b. if a house is vacant, who are the candidates who qualify? list them
+           in decending order of priority
+ */
+
+/**
+ * @author anonza
+ * Date : 27 March 2024
+ */
+public class tenants {
+    
+}
